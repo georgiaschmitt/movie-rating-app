ac611976456369ee6495c9dbe60e50456da1caf2
Jade Le <baongocle1991@gmail.com>
2020-10-08 13:40:46 -0700

    completed make_melons function
    Co-authored-by: alishahwee <alisha.hwee@outlook.com>

--- oo-practice-melons/harvest.py
+++ oo-practice-melons/harvest.py
@@ -85,19 +85,43 @@ def make_melon_type_lookup(melon_types):
 # Part 2   #
 ############
 
-class Melon(object):
+class Melon(object):  
     """A melon in a melon harvest."""
 
     # Fill in the rest
     # Needs __init__ and is_sellable methods
+    def __init__(self, melon_type, shape_rating, color_rating, harvested_from, harvested_by):
+        self.melon_type = melon_type
+        self.shape_rating = shape_rating
+        self.color_rating = color_rating
+        self.harvested_from = harvested_from
+        self.harvested_by = harvested_by
+
+    def is_sellable(self):
+        return True if self.shape_rating > 5 and self.color_rating > 5 and self.harvested_from != "Field 3" else False
+
 
 def make_melons(melon_types):
     """Returns a list of Melon objects."""
 
     # Fill in the rest
+    melons_by_id = make_melon_type_lookup(melon_types)
+
+    melon_1 = Melon(melons_by_id['yw'], 8, 7, "Field 2", "Sheila")
+    melon_2 = Melon(melons_by_id['yw'], 3, 4, "Field 2", "Sheila")
+    melon_3 = Melon(melons_by_id['yw'], 9, 8, "Field 3", "Sheila")
+    melon_4 = Melon(melons_by_id['cas'], 10, 6, "Field 35", "Sheila")
+    melon_5 = Melon(melons_by_id['cren'], 8, 9, "Field 35", "Michael")
+    melon_6 = Melon(melons_by_id['cren'], 8, 2, "Field 35", "Michael")
+    melon_7 = Melon(melons_by_id['cren'], 2, 3, "Field 4", "Michael")
+    melon_8 = Melon(melons_by_id['musk'], 6, 7, "Field 4", "Michael")
+    melon_9 = Melon(melons_by_id['yw'], 7, 10, "Field 3", "Sheila")
+    
+    return [melon_1, melon_2, melon_3, melon_4, melon_5, melon_6, melon_7, melon_8, melon_9]
 
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
+    
 
     # Fill in the rest 
 
