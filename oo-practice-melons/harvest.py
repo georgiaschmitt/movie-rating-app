15c8eeedc964eca686825617b692e4a32b97be5d
Jade Le <baongocle1991@gmail.com>
2020-10-08 13:01:03 -0700

    completed part 1
    Co-authored-by: alishahwee <alisha.hwee@outlook.com>"

--- oo-practice-melons/harvest.py
+++ oo-practice-melons/harvest.py
@@ -13,16 +13,24 @@ class MelonType(object):
         self.pairings = []
 
         # Fill in the rest
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
 
         # Fill in the rest
+        self.pairings.append(pairing)
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
 
         # Fill in the rest
+        self.code = new_code
 
 
 def make_melon_types():
@@ -31,18 +39,47 @@ def make_melon_types():
     all_melon_types = []
 
     # Fill in the rest
+    # fido = Animal('Fido')   # making an object ("instantiating")
+    musk = MelonType('musk', 1998, 'green', True, True, 'Muskmelon')
+    musk.add_pairing('mint')
+    all_melon_types.append(musk)
+
+    cas = MelonType('cas', 2003, 'orange', False, False, 'Casaba')
+    cas.add_pairing('strawberry')
+    cas.add_pairing('mint')
+    all_melon_types.append(cas)
+    
+    cren = MelonType('cren', 1996, 'green', False, False, 'Crenshaw')
+    cren.add_pairing('proscuitto')
+    all_melon_types.append(cren)
+
+    yw = MelonType('yw', 2013, 'yellow', False, True, 'Yellow Watermelon')
+    yw.add_pairing('ice cream')
+    all_melon_types.append(yw)
+
 
     return all_melon_types
 
+
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
-
-    # Fill in the rest
+    
+    for melon in melon_types:
+        print(f'{melon.name} pairs with')        
+        for pair in melon.pairings:
+            print(f'-{pair}')
+        print()
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
-    # Fill in the rest
+    melons_dict = {}
+
+    for melon in melon_types:
+        melons_dict[melon.code] = melon
+
+    return melons_dict
+
 
 ############
 # Part 2   #
