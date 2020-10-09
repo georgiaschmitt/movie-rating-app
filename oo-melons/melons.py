7abd874ea46851caef81c8930a03500a467cc1c3
Jade Le <baongocle1991@gmail.com>
2020-10-09 10:09:33 -0700

    completed p1 and p2, created AbstractMelonOrder class
    Co-authored-by georgiaschmitt <georgia.schmitt@gmail.com>

---
+++ oo-melons/melons.py
@@ -0,0 +1,54 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder():
+    """An abstract base class that other Melon Orders inherit from."""
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+        self.species = species
+        self.qty = qty
+    
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5 * 1.5
+        total = (1 + self.tax) * self.qty * base_price
+        
+        if self.order_type == 'international' and self.qty < 10:
+            total += 3
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+        super().__init__(species, qty)
+        self.shipped = False
+        self.order_type = "domestic"
+        self.tax = 0.08
+    
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes."""
+
+        super().__init__(species, qty)
+        self.country_code = country_code
+        self.shipped = False
+        self.order_type = "international"
+        self.tax = 0.17
+
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
