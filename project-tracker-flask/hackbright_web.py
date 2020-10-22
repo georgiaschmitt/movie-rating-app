ec8afc13fc2f70a40cd9eccbf892e9e7c608e1e7
Jade Le <baongocle1991@gmail.com>
2020-10-22 07:40:42 +0000

    created student forma nd grade listing for student

---
+++ project-tracker-flask/hackbright_web.py
@@ -0,0 +1,50 @@
+"""A web application for tracking projects, students, and student grades."""
+
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get('github')
+
+    first, last, github = hackbright.get_student_by_github(github)
+
+    grades = hackbright.get_grades_by_github(github)
+
+    return render_template("student_info.html", first=first, last=last, github=github, grades=grades)
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/student-add-form")
+def student_add_form():
+
+    return render_template("add_student.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""  
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    github = request.form.get('github')
+
+    return render_template('add_student_result.html', first_name=first_name, last_name=last_name, github=github)
+
+
+
+
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True, host="0.0.0.0")
